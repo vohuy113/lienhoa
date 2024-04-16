5eabe297e0612dbb19b788f8655b7c70e2adf0eb
Vo Xuan Huy <vohuy1103@gmail.com>
2024-04-16 13:40:50 +0700

    fix:log Version

--- setDateBuild.js
+++ setDateBuild.js
@@ -1,36 +1,13 @@
 import fs from "fs";
 
-let envContent = "";
-try {
-  envContent = fs.readFileSync(".env", "utf8");
-} catch (err) {
-  console.error("Error reading .env file:", err);
-  process.exit(1);
-}
-
-const now = new Date();
-const currentDateTime = now
+const currentDate = new Date().toLocaleString();
+const currentDateTime = currentDate
   .toLocaleString("vi-VN", {
     timeZone: "Asia/Ho_Chi_Minh",
   })
   .toString();
+fs.writeFileSync("build-date.txt", currentDateTime);
+console.log("Ứng dụng được build vào ngày sss:", currentDateTime);
+const buildDate = import.meta.env.__BUILD_DATE__;
 
-let newEnvContent = "";
-if (envContent.includes("REACT_APP_CURRENT_DATETIME")) {
-  newEnvContent = envContent.replace(
-    /REACT_APP_CURRENT_DATETIME="[^"]*"/,
-    `REACT_APP_CURRENT_DATETIME="${currentDateTime}"`
-  );
-} else {
-  newEnvContent = `${envContent}\nREACT_APP_CURRENT_DATETIME="${currentDateTime}"`;
-}
-
-try {
-  fs.writeFileSync(".env", newEnvContent);
-  console.log(
-    "Environment variable REACT_APP_CURRENT_DATETIME added/updated in .env file."
-  );
-} catch (err) {
-  console.error("Error writing to .env file:", err);
-  process.exit(1);
-}
+console.log("Ứng dụng được build vào ngày:", buildDate);
