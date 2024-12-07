1e4f3ba83d520382f0cbdc64f6ed42c88c7b6d76
Vo Xuan Huy <vohuy1103@gmail.com>
2024-12-07 23:41:59 +0700

    add hello

--- src/components/CardAbout.jsx
+++ src/components/CardAbout.jsx
@@ -4,6 +4,7 @@ const CardAbout = ({ first, second }) => {
   return (
     <div className=" py-4 border-solid border  border-[#000] flex flex-col items-center justify-center">
       <span className="text-[32px] font-semibold">{first}</span>
+      <div>Hello</div>
       <p className="text-[14px]">{second}</p>
     </div>
   );
